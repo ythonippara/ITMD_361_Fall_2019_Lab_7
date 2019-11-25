56c791d2d9023a18f9b758a5c157265705ed21b1
ythonippara <47587277+ythonippara@users.noreply.github.com>
2019-11-24 22:27:44 -0600

    Fixing errors

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 function init(){
   //add your JavaScript between these two lines of code
-  var button = document.getElementByID('entrybutton');
+  var button = document.getElementById('entrybutton');
   
   function showSomeText(){
-    var texfield = document.getElementByID('entryinput');
-    document.getElementByID('textoutput').innerHTML = textfield.value;
+    var texfield = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = textfield.value;
     alert("Yulia Thonippara: " + textfield.value);
   }
   
