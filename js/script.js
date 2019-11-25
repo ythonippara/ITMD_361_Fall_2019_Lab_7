484fc43978ec5dfe8d55130f508de87699c1fe5b
ythonippara <47587277+ythonippara@users.noreply.github.com>
2019-11-25 00:14:44 -0600

    Eliminating button variable

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
-function init(){
+function init() {
   //add your JavaScript between these two lines of code
   
-  var button = document.getElementById('entrybutton');
+  document.getElementById("entrybutton").addEventListener("click", showConfirmationDialog);
   
   function showConfirmationDialog() {
-    var texfield = document.getElementById('entryinput');
+    var texfield = document.getElementById("entryinput");
     alert("Yulia Thonippara: " + textfield.value);
-    document.getElementById('textoutput').innerHTML = textfield.value;
+    document.getElementById("textoutput").innerHTML = textfield.value;
   }
-  
-  button.addEventListener('click', showConfirmationDialog);
 }
 
-window.addEventListener('load', init);
+window.addEventListener("load", init);
