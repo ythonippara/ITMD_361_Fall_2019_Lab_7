64084a5d7235f79f376860b84b2197922fe2ee61
ythonippara <47587277+ythonippara@users.noreply.github.com>
2019-11-24 22:19:51 -0600

    Deleted extra space

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function init(){
   //add your JavaScript between these two lines of code
   var button = document.getElementByID('entrybutton');
   
-  function showSomeText() {
+  function showSomeText(){
     var texfield = document.getElementByID('entryinput');
     document.getElementByID('textoutput').innerHTML = textfield.value;
     alert("Yulia Thonippara: " + textfield.value);
