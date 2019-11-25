ad56745ae936ae3cbf830b379567e12e007d5b1b
ythonippara <47587277+ythonippara@users.noreply.github.com>
2019-11-24 22:18:04 -0600

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 function init(){
-//add your javascript between these two lines of code
- 
-
-
-
-
-
-
+  //add your JavaScript between these two lines of code
+  var button = document.getElementByID('entrybutton');
+  
+  function showSomeText() {
+    var texfield = document.getElementByID('entryinput');
+    document.getElementByID('textoutput').innerHTML = textfield.value;
+    alert("Yulia Thonippara: " + textfield.value);
+  }
+  
+  button.addEventListener('click', showSomeText);
+}
 
 window.addEventListener('load', init);
