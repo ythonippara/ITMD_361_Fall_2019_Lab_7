411ac5b80b12a1967958a6975acf93abb76b25a2
ythonippara <47587277+ythonippara@users.noreply.github.com>
2019-11-24 23:36:27 -0600

    Removing button variable from the function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function init(){
   //add your JavaScript between these two lines of code
   
+  var button = document.getElementById('entrybutton');
+  
   button.addEventListener('click', showMeSomeText);
   
   function showMeSomeText() {
     var texfield = document.getElementById('entryinput');
-    var button = document.getElementById('entrybutton');
     alert("Yulia Thonippara: " + textfield.value);
     document.getElementById('textoutput').innerHTML = textfield.value;
   }
